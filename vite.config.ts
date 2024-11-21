883ea742e637810737c49d477ef235310b804a6d
Nikita Prokopov <nktprggl@gmail.com>
2024-11-21 20:34:57 +0300

    chore: configure TypeScript path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
+import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./src"),
+    },
+  },
+
   plugins: [react(), svgr()],
 });
