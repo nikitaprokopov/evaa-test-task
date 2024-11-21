f36e95d975820b56618ce5e7fc2a269be2f638c6
Nikita Prokopov <nktprggl@gmail.com>
2024-11-21 21:01:59 +0300

    chore: configure ESLint for code quality

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,65 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from "eslint-config-prettier";
+import perfectionist from "eslint-plugin-perfectionist";
+import reactRefresh from "eslint-plugin-react-refresh";
+import reactHooks from "eslint-plugin-react-hooks";
+import tseslint from "typescript-eslint";
+import react from "eslint-plugin-react";
+import eslint from "@eslint/js";
+import globals from "globals";
 
 export default tseslint.config(
-  { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    ignores: ["dist"],
+  },
+
+  {
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.strictTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+      perfectionist.configs["recommended-line-length"],
+      eslintConfigPrettier,
+    ],
+
+    files: ["**/*.{ts,tsx}"],
+
     languageOptions: {
-      ecmaVersion: 2020,
+      parserOptions: {
+        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+        tsconfigRootDir: import.meta.dirname,
+        ecmaFeatures: { jsx: true },
+        projectService: true,
+      },
+
       globals: globals.browser,
+      ecmaVersion: "latest",
     },
+
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-refresh": reactRefresh,
+      "react-hooks": reactHooks,
+      react,
     },
+
+    settings: {
+      react: { version: "detect" },
+    },
+
     rules: {
+      ...react.configs.recommended.rules,
+      ...react.configs["jsx-runtime"].rules,
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+
+      // React
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      "react/no-unescaped-entities": 0,
+
+      // TS
+      "@typescript-eslint/array-type": ["error", { default: "array-simple", readonly: "array-simple" }],
+      "@typescript-eslint/no-confusing-void-expression": 0,
+
+      // Perfectionist
+      "perfectionist/sort-modules": "off",
     },
   },
-)
+);
