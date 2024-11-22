9aef10cea4925888433b5c93a9f8c683118752e6
Nikita Prokopov <nktprggl@gmail.com>
2024-11-22 21:04:55 +0300

    feat: add buffer polyfill for compatibility

---
+++ src/polyfills.ts
@@ -0,0 +1,9 @@
+/* eslint-disable */
+import { Buffer } from "buffer";
+
+globalThis.Buffer = Buffer;
+(window as any).global = window;
+
+(BigInt.prototype as any).toJSON = function () {
+  return this.toString();
+};
