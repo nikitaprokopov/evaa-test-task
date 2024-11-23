7f4fc3a9ae0729f664d4cdfd5dc6643155553089
Nikita Prokopov <nktprggl@gmail.com>
2024-11-23 18:15:36 +0300

    chore: uninstall @reatom/devtools

--- src/libs/reatom/reatom-ctx.ts
+++ src/libs/reatom/reatom-ctx.ts
@@ -1,4 +1,3 @@
-import { connectDevtools } from "@reatom/devtools";
 import { connectLogger } from "@reatom/logger";
 import { createCtx } from "@reatom/framework";
 
@@ -8,5 +7,4 @@ export const reatomCtx = createCtx();
 
 if (ENV.DEV) {
   connectLogger(reatomCtx);
-  connectDevtools(reatomCtx);
 }
