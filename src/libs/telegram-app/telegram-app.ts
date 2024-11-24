5ac7764d1f64b79620a3b1333596d051ad85f52c
Nikita Prokopov <nktprggl@gmail.com>
2024-11-24 18:01:26 +0300

    feat: configure telegram app behavior

--- src/libs/telegram-app/telegram-app.ts
+++ src/libs/telegram-app/telegram-app.ts
@@ -1,4 +1,24 @@
-import { mockTelegramEnv, isTMA, init } from "@telegram-apps/sdk-react";
+import { mockTelegramEnv, closingBehavior, swipeBehavior, isTMA, init } from "@telegram-apps/sdk-react";
+
+function enableConfirmationOnClose() {
+  if (closingBehavior.mount.isAvailable()) {
+    closingBehavior.mount();
+
+    if (closingBehavior.enableConfirmation.isAvailable()) {
+      closingBehavior.enableConfirmation();
+    }
+  }
+}
+
+export function disableVerticalSwipe() {
+  if (swipeBehavior.mount.isAvailable()) {
+    swipeBehavior.mount();
+
+    if (swipeBehavior.disableVertical.isAvailable()) {
+      swipeBehavior.disableVertical();
+    }
+  }
+}
 
 export async function initTelegramApp() {
   const isTma = await isTMA();
@@ -12,4 +32,7 @@ export async function initTelegramApp() {
   }
 
   init();
+
+  enableConfirmationOnClose();
+  disableVerticalSwipe();
 }
